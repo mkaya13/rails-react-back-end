651510edadb4c56120994e65a946a93010616042
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-28 21:52:00 +0300

    Adds the back-end layer of the application

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Message.create(message: 'Good Morning!')
+Message.create(message: 'Good Afternoon!')
+Message.create(message: 'Good Evening!')
+Message.create(message: 'Good Night!')
+Message.create(message: 'Good to see you Again!')
